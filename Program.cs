af6228a2588c63304760473d51af71c1e53f0e25
kjack444444 <123431769+kjack444444@users.noreply.github.com>
2023-01-23 23:39:39 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Homework1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double x =2.5;
+        double y =3.3;
+        double z;
+        z= 4*x*x+3*y;
+        Console.Write("X= "+x);
+        Console.Write("\tY= "+y);
+        Console.Write("The Value of Z is: "+z);
+    }
+}
