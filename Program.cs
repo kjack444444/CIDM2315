dbfc91604b8b57dd0bdc5cd8a9539cdde721d54a
kjack444444 <123431769+kjack444444@users.noreply.github.com>
2023-01-30 21:23:49 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Homework2pt2;
+class Program
+{
+    static void Main(string[] args)
+   {
+    Console.WriteLine("Please input the first num:");
+    int a=Convert.ToInt16(Console.ReadLine()); 
+    Console.WriteLine("Please input the second num:");
+    int b=Convert.ToInt16(Console.ReadLine());
+    Console.WriteLine("Please input the third num:");
+    int c=Convert.ToInt16(Console.ReadLine());
+    int smallest=0;
+    if(a<b) {
+        if(a<c) {
+            smallest=a;
+        }
+    }
+    if(b<a) {
+        if(b<c) {
+            smallest=b;
+        }
+    }
+    if(c<a) {
+        if(c<b) {
+            smallest=c;
+        }
+    }
+    Console.WriteLine("The smallest value is: {0}",smallest);
+  }
+
+}
