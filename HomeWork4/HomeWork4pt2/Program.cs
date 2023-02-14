f99c1f5d9f13575f85760ddb150030b73d88618f
kjack444444 <123431769+kjack444444@users.noreply.github.com>
2023-02-13 22:42:50 -0600

    Add files via upload

---
+++ HomeWork4/HomeWork4pt2/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace HomeWork4pt2;
+class Program
+{
+    static void Main(string[] args)
+    {
+      int N = 5; 
+   string shape; 
+   
+   Console.Write("Input shape: ");
+   shape= Console.ReadLine(); 
+   Console.Write("N is: "+N+"; shape is "+shape+"\n"); 
+   pattern(N,shape); 
+}
+static void pattern(int N,string shape){
+    int i,j,k; 
+   if(shape=="left"){
+    
+   for(i=1;i<=N;i++)
+       {
+   for(j=1;j<=i;j++)
+    	   Console.Write("*");
+    	Console.Write("\n"); 
+       }
+      }
+    else if(shape=="right"){
+    
+   for (i = 1; i <= N; i++)  
+         {  
+   for (j = 1; j <= N-i; j++)  
+   {Console.Write(" ");}  
+   for (k = 1; k <= i; k++)  
+   {  Console.Write("*");}  
+            Console.WriteLine("");  }  
+        Console.ReadLine();}
+    }
+}
---
+++ HomeWork4/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace HomeWork4;
+class Program
+{
+    static void Main(string[] args)
+    {
+       int num1 =3; int num2 =5;
+       Console.WriteLine($"A={num1} B={num2}");
+       int large = LargestNum(num1,num2);
+       Console.Write("\nLargest Number is:"+large);
+    }
+    static int LargestNum(int num1, int num2){
+        if(num1>num2){
+            return num1;
+        }
+        else if(num2>num1){
+            return num2;
+        }
+        else{
+            return -1;}
+
+    }
+}
