7c84c5afb4057346af3813f8906a57dd9331d24e
kjack444444 <123431769+kjack444444@users.noreply.github.com>
2023-02-13 22:41:19 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,27 +1,36 @@
-﻿namespace Homework3;
+﻿namespace HomeWork4pt2;
 class Program
 {
     static void Main(string[] args)
     {
-       bool flag = true;
-    Console.WriteLine("Input an integer: ");
-    string input = Console.ReadLine();
-    int number = Convert.ToInt32(input);
-    for(int i=2;i<number/2;i++)
-    {
-        if(number%i==0)
-        {
-               flag = false;
-               break;
-        }
-    }
-    if(flag)
-    {
-        Console.WriteLine( input +" is Prime ");
-    }
-    else{
-        Console.WriteLine(input+" is non-prime ");
-    }
+      int N = 5; 
+   string shape; 
+   
+   Console.Write("Input shape: ");
+   shape= Console.ReadLine(); 
+   Console.Write("N is: "+N+"; shape is "+shape+"\n"); 
+   pattern(N,shape); 
+}
+static void pattern(int N,string shape){
+    int i,j,k; 
+   if(shape=="left"){
     
-  }
+   for(i=1;i<=N;i++)
+       {
+   for(j=1;j<=i;j++)
+    	   Console.Write("*");
+    	Console.Write("\n"); 
+       }
+      }
+    else if(shape=="right"){
+    
+   for (i = 1; i <= N; i++)  
+         {  
+   for (j = 1; j <= N-i; j++)  
+   {Console.Write(" ");}  
+   for (k = 1; k <= i; k++)  
+   {  Console.Write("*");}  
+            Console.WriteLine("");  }  
+        Console.ReadLine();}
+    }
 }
