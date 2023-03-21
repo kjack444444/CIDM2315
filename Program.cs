91cbbb8a1e71e2ef8d43259f9b4e381cd78628e5
kjack444444 <123431769+kjack444444@users.noreply.github.com>
2023-03-20 22:24:00 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,36 +1,42 @@
-﻿namespace HomeWork4pt2;
+﻿namespace HomeWork7;
 class Program
 {
     static void Main(string[] args)
     {
-      int N = 5; 
-   string shape; 
-   
-   Console.Write("Input shape: ");
-   shape= Console.ReadLine(); 
-   Console.Write("N is: "+N+"; shape is "+shape+"\n"); 
-   pattern(N,shape); 
-}
-static void pattern(int N,string shape){
-    int i,j,k; 
-   if(shape=="left"){
-    
-   for(i=1;i<=N;i++)
-       {
-   for(j=1;j<=i;j++)
-    	   Console.Write("*");
-    	Console.Write("\n"); 
-       }
-      }
-    else if(shape=="right"){
-    
-   for (i = 1; i <= N; i++)  
-         {  
-   for (j = 1; j <= N-i; j++)  
-   {Console.Write(" ");}  
-   for (k = 1; k <= i; k++)  
-   {  Console.Write("*");}  
-            Console.WriteLine("");  }  
-        Console.ReadLine();}
+        Customer c1=new Customer(110,"Alice",28);
+  Customer c2=new Customer(111,"Bob",30);
+  c1.PrintCustInfo();
+  c2.PrintCustInfo();
+  Console.WriteLine();
+  c1.ChangeID(220);
+  c2.ChangeID(221);
+/*This is where we will print the info of the cust*/
+  c1.PrintCustInfo();
+  c2.PrintCustInfo();
+  Console.WriteLine("Bob is older");
     }
+    class Customer{
+
+private int cust_id;
+public string cust_name;
+public int cust_age;
+
+public Customer(int id,string name,int age)
+{
+  cust_id=id;
+  cust_name=name;
+  cust_age=age;
 }
+public void ChangeID(int new_id)
+{
+  cust_id=new_id;
+}
+public void PrintCustInfo()
+{
+  Console.WriteLine("Customer's ID: {0}, Customer's Name:{1}, Customer's Age:{2}",cust_id,cust_name,cust_age);
+}
+    }
+   
+}
+
+
